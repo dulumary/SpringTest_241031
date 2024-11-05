974a06380000a1ebf53e515dfabc884e5835b719
dulumary <lecture@hagulu.com>
2024-11-05 18:25:55 +0900

    [김인규]  - mybatis select 문제 풀이

---
+++ src/main/java/com/marondal/spring/test/database/service/StoreService.java
@@ -0,0 +1,24 @@
+package com.marondal.spring.test.database.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marondal.spring.test.database.domain.Store;
+import com.marondal.spring.test.database.repository.StoreRepository;
+
+@Service
+public class StoreService {
+	
+	@Autowired
+	private StoreRepository storeRepository;
+	
+	// 가게 리스트 얻어오기 
+	public List<Store> getStoreList() {
+		List<Store> storeList = storeRepository.selectStoreList();
+		
+		return storeList;
+	}
+
+}
---
+++ src/main/java/com/marondal/spring/test/mybatis/repository/RealEstateRepository.java
@@ -0,0 +1,24 @@
+package com.marondal.spring.test.mybatis.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.marondal.spring.test.mybatis.domain.RealEstate;
+
+@Mapper
+public interface RealEstateRepository {
+	
+	// 전달받은 id와 일치하는 real_estate 행 조회 
+	public RealEstate selectRealEstate(@Param("id") int id);
+	
+	
+	// 전달받은 월세보다 낮은 행 조회 
+	public List<RealEstate> selectRealEstateByRentPrice(@Param("rentPrice") int rentPrice);
+	
+	public List<RealEstate> selectRealEstateByAreaAndPrice(
+			@Param("area") int area
+			, @Param("price") int price);
+
+}
