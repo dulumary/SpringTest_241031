a65e495c926f8cc0045155ba6d9a032b5c4f52bc
dulumary <lecture@hagulu.com>
2024-11-07 18:48:30 +0900

    [김인규]  - mvc cycle 문제 풀이

---
+++ src/main/java/com/marondal/spring/test/mvc/SellerController.java
@@ -0,0 +1,57 @@
+package com.marondal.spring.test.mvc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.marondal.spring.test.mvc.domain.Seller;
+import com.marondal.spring.test.mvc.service.SellerService;
+
+@RequestMapping("/mvc/seller")
+@Controller
+public class SellerController {
+	
+	@Autowired
+	private SellerService sellerService;
+	
+	@PostMapping("/create")
+	public String  createSeller(
+			@RequestParam("nickname") String nickname
+			, @RequestParam("profileImage") String profileImage
+			, @RequestParam("temperature") double temperature) {
+		
+		int count = sellerService.addSeller(nickname, profileImage, temperature);
+		
+		return "redirect:/mvc/seller/info";
+	}
+	
+	@GetMapping("/input")
+	public String sellerInput() {
+		return "mvc/sellerInput";
+	}
+	
+	@GetMapping("/info")
+	public String sellerInfo(
+			@RequestParam(value="id", required=false) Integer id
+			, Model model) {
+		
+		Seller seller = null;
+		if(id == null) {			
+			seller = sellerService.getLastSeller();
+			
+		} else {
+			seller = sellerService.getSeller(id);
+		}
+		
+		model.addAttribute("seller", seller);
+		
+		return "mvc/sellerInfo";
+	}
+	
+
+}
---
+++ src/main/java/com/marondal/spring/test/mvc/repository/SellerRepository.java
@@ -0,0 +1,20 @@
+package com.marondal.spring.test.mvc.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.marondal.spring.test.mvc.domain.Seller;
+
+@Mapper
+public interface SellerRepository {
+	
+	public int insertSeller(
+			@Param("nickname") String nickname
+			, @Param("profileImage") String profileImage
+			, @Param("temperature") double temperature);
+	
+	public Seller selectLastSeller();
+	
+	public Seller selectSeller(@Param("id") int id);
+
+}
