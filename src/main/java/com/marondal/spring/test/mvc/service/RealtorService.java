8925a0f5bbc718b70603178f2ac7703c24f6e6cb
dulumary <lecture@hagulu.com>
2024-11-08 18:17:49 +0900

    [김인규]  - useGerneratedkey 문제 풀이

---
+++ src/main/java/com/marondal/spring/test/mvc/service/RealtorService.java
@@ -0,0 +1,21 @@
+package com.marondal.spring.test.mvc.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marondal.spring.test.mvc.domain.Realtor;
+import com.marondal.spring.test.mvc.repository.RealtorRepository;
+
+@Service
+public class RealtorService {
+	
+	@Autowired
+	private RealtorRepository realtorRepository;
+	
+	public int addRealtor(Realtor realtor) {
+		int count = realtorRepository.insertRealtor(realtor);
+		
+		return count;
+	}
+
+}
