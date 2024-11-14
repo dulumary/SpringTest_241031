1e34f77a5d207871e6056a6cf34641fba1977067
dulumary <lecture@hagulu.com>
2024-11-14 18:49:44 +0900

    [김인규]  - 날씨 문제 풀이

--- src/main/java/com/marondal/spring/test/thymeleaf/WeatherController.java
+++ src/main/java/com/marondal/spring/test/thymeleaf/WeatherController.java
@@ -1,21 +1,67 @@
 package com.marondal.spring.test.thymeleaf;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.marondal.spring.test.thymeleaf.domain.Weather;
+import com.marondal.spring.test.thymeleaf.service.WeatherService;
 
 @RequestMapping("/thymeleaf/weather")
 @Controller
 public class WeatherController {
 	
+	@Autowired
+	private WeatherService weatherService;
+	
 	@GetMapping("/list")
-	public String weatherHistory() {
+	public String weatherHistory(Model model) {
+		
+		// 날씨 기록 리스트를 얻어온다. 
+		List<Weather> weatherList = weatherService.getWeatherList();
+		
+		// html에서 사용할 데이터 
+		// model 에 추가
+		model.addAttribute("weatherList", weatherList);
+		
 		return "thymeleaf/weather/list";
+	}
+	
+	@GetMapping("/create")
+	public String createWeather(
+//			@DateTimeFormat(pattern="yyyy년 M월 d일")
+//			@RequestParam("date") LocalDate date
+//			, @RequestParam("weather") String weather
+//			, @RequestParam("temperatures") double temperatures
+//			, @RequestParam("precipitation") double precipitation
+//			, @RequestParam("microDust") String microDust
+//			, @RequestParam("windSpeed") double windSpeed) {
+			 @ModelAttribute Weather weather){
+		
+//		int count = weatherService.addWeather(date, weather, temperatures, precipitation, microDust, windSpeed);
+		
+		int count = weatherService.addWeather(
+				weather.getDate()
+				, weather.getWeather()
+				, weather.getTemperatures()
+				, weather.getPrecipitation()
+				, weather.getMicroDust()
+				, weather.getWindSpeed());
+		
+		
+		return "redirect:/thymeleaf/weather/list";
 	}
 	
 	@GetMapping("/input")
 	public String weatherInput() {
 		return "thymeleaf/weather/input";
 	}
+	
+	
 
 }
---
+++ src/main/java/com/marondal/spring/test/thymeleaf/service/WeatherService.java
@@ -0,0 +1,43 @@
+package com.marondal.spring.test.thymeleaf.service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marondal.spring.test.thymeleaf.domain.Weather;
+import com.marondal.spring.test.thymeleaf.repository.WeatherRepository;
+
+@Service
+public class WeatherService {
+	@Autowired
+	private WeatherRepository weatherRepository;
+	
+	public List<Weather>  getWeatherList() {
+		
+		List<Weather> weatherList = weatherRepository.selectWeatherHistory();
+		
+		return weatherList;
+		
+	}
+	
+	public int addWeather(
+			LocalDate date
+			, String weather
+			, double temperatures
+			, double precipitation
+			, String microDust
+			, double windSpeed) {
+		
+		int count = weatherRepository.insertWeather(date, weather, temperatures, precipitation, microDust, windSpeed);
+		
+		return count;
+	}
+
+}
+
+
+
+
+
